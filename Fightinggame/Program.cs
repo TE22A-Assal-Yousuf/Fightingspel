4a7ec909b1b7b540f284529998a633dafd463564
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-09-21 10:56:15 +0200

    cool stuff

--- Fightinggame/Program.cs
+++ Fightinggame/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//RNG
+Random genertaotor = new Random();
+
+int Armour = 20;
+int RNG = genertaotor.Next(10, 100);
+int finaldamage = RNG - Armour;
+
+if (finaldamage <= 0){
+    Console.WriteLine("you died");
+}
+else{
+    Console.WriteLine(finaldamage);
+}
+
+//Loop
+
+int tal = 100;
+
+while (tal > 0){
+    Console.WriteLine(tal);
+    tal--;
+}
+
+Console.ReadLine();
